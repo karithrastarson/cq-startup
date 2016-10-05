26632dc4c2360404ceb30200d4ed9b64be6cf5d7
Kári <karithrastarson@gmail.com>
2016-10-05 10:29:01 +0200

    New Archetype

---
+++ cqStartup/src/main/java/com/cqstartup/App.java
@@ -0,0 +1,13 @@
+package com.cqstartup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
